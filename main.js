1ba09ce3deb811387a21c6dccd6daacfa437e74a
Rafi Noor Islami <rafi22599@gmail.com>
2023-05-08 04:12:33 +0700

    No assets
    add html and js files

---
+++ main.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            nama: "Rafi Noor Islami",
+            NIM: "2114311029",
+            image: "assets/images/WhatsApp Image 2023-04-29 at 11.20.26 PM-modified.png",
+            alt: "Rafi Photo",
+            bio: "Hello👋",
+            hide: true
+        }
+    },
+    methods: {
+        hideContent() {
+            if (this.hide == true) {
+                this.hide = false 
+            } else {
+                this.hide = true
+            }
+        }
+        
+    }
+})
