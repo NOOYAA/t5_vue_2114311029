c39975f7f7515fefbe39185f2862185ecc7141fd
Rafi Noor Islami <rafi22599@gmail.com>
2023-05-09 21:36:55 +0700

    Update main.js
    Image path update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             nama: "Rafi Noor Islami",
             NIM: "2114311029",
-            image: "assets/images/WhatsApp Image 2023-04-29 at 11.20.26 PM-modified.png",
+            image: "WhatsApp Image 2023-04-29 at 11.20.26 PM-modified.png",
             alt: "Rafi Photo",
             bio: "Hello👋",
             existence: true,
